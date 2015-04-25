8f91fdc26c831a1f50ce192082e9db832f5abbd9
bcdady <bryan+git@dady.us>
2015-04-25 12:19:13 -0600

    Fix copy-module.ps1
    Remove 'useless' executionpolicy check.
    Move target Modules directory to be in ProgramFiles, instead of in the
    user's home directory, so the Module could be accessed by other users
    e.g. on a server

--- copy-module.ps1
+++ copy-module.ps1
@@ -2,22 +2,15 @@
 # File Name : copy-module.ps1
 # Author    : Bryan Dady; @bcdady
 
-if (Get-ExecutionPolicy -eq 'Undefined') {
-    Write-Warning 'PowerShell ExecutionPolicy  is Undefined, and so this script won`'t run as intended.`nChange the policy to Unrestricted and/or run `'get-help about_Execution_Policies`' for more info.'
-    
-} else {
-
 # get the name of current directory, as it's likely the name of the module to be installed
 $dirName = Resolve-Path -Path $pwd | Split-Path -Leaf;
 
-# mkdir a new directory for this module in the default user PS Modules directory; creating the rest of the path, if necesarry
-New-Item -Path $env:USERPROFILE\Documents\WindowsPowerShell\Modules -Name $dirName -ItemType Directory -Force
+# Create a new directory for this module in the ProgramFiles PS Modules directory, so it can be shared by any/all PowerShell users on the host OS
+New-Item -Path "$env:ProgramFiles\WindowsPowerShell\Modules" -Name $dirName -ItemType Directory -Force
 
-# write-output  -InputObject "Created $env:USERPROFILE\Documents\WindowsPowerShell\Modules\$dirName";
-write-output  -InputObject "Copying module to $dirName\";
+write-output  -InputObject "Copying module to $dirName\`n";
 
 Get-ChildItem $pwd -Recurse -Exclude 'copy-module.ps1','.git*','readme.md' | 
 Copy-Item -Destination $env:USERPROFILE\Documents\WindowsPowerShell\Modules\$dirName;
 
 Write-Output -InputObject "`n # Congratulations! Module $dirName is now ready to be imported.`n To load the module, so you can start using it, run 'import-module -name $dirName -verbose'`n`n"
-}
