f50b6020df2135b3f2695e6f0be118b7f10c07a8
bcdady <bryan+git@dady.us>
2015-04-25 20:24:23 -0600

    Improved copy-module
    Improved copy-module to be more portable, by working from $PSScriptRoot
    ProfilePal.psd1 should now be ASCii text syntax

--- copy-module.ps1
+++ copy-module.ps1
@@ -2,15 +2,26 @@
 # File Name : copy-module.ps1
 # Author    : Bryan Dady; @bcdady
 
-# get the name of current directory, as it's likely the name of the module to be installed
-$dirName = Resolve-Path -Path $pwd | Split-Path -Leaf;
+# Get the name of this script's directory, as it's likely the name of the module to be installed
+$dirName = Resolve-Path -Path $PSScriptRoot | Split-Path -Leaf;
+
+# Test Admin permissions; if found, install module for all users. If NOT, install for current user only
+if (([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+    [Security.Principal.WindowsBuiltInRole] 'Administrator'))
+{
+    $moduleDestination = "$env:ProgramFiles\WindowsPowerShell\Modules";
+    $installAllUsers = $true
+} else {
+    $moduleDestination = "$env:USERPROFILE\Documents\WindowsPowerShell\Modules";
+    $installAllUsers = $false
+}
 
 # Create a new directory for this module in the ProgramFiles PS Modules directory, so it can be shared by any/all PowerShell users on the host OS
-New-Item -Path "$env:ProgramFiles\WindowsPowerShell\Modules" -Name $dirName -ItemType Directory -Force
+New-Item -Path $moduleDestination -Name $dirName -ItemType Directory -Force
 
-write-output  -InputObject "Copying module to $dirName\`n";
+write-output  -InputObject "Copying module to $moduleDestination\$dirName\`n";
 
-Get-ChildItem $pwd -Recurse -Exclude 'copy-module.ps1','.git*','readme.md' | 
-Copy-Item -Destination $env:USERPROFILE\Documents\WindowsPowerShell\Modules\$dirName;
+Get-ChildItem $PSScriptRoot -Recurse -Exclude 'copy-module.ps1','.git*','*.md' | 
+Copy-Item -Destination $moduleDestination\$dirName ;
 
 Write-Output -InputObject "`n # Congratulations! Module $dirName is now ready to be imported.`n To load the module, so you can start using it, run 'import-module -name $dirName -verbose'`n`n"
